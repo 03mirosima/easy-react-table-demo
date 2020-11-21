f585b2e917fde27e7a4361da963b8769e547f26b
03mirosima <sezenmiray2009@hotmail.com>
2020-11-21 13:43:01 +0300

    initial commit

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Ad',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Soyad',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Doğum Tarihi',
+        accessor: 'date_of_birth'
+    },
+    {
+        Header: 'Ülke',
+        accessor: 'country'
+    },
+    {
+        Header: 'Telefon',
+        accessor: 'phone'
+    }
+]
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BasicTable } from './components/BasicTable';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BasicTable/>
   </React.StrictMode>,
   document.getElementById('root')
 );
